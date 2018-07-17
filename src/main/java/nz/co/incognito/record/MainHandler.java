e130f5872655a7c538b220d8785cf29576ce838b
Jack Tan <jacktan165@gmail.com>
2018-07-18 00:31:06 +1200

    Init project

---
+++ src/main/java/nz/co/incognito/record/MainHandler.java
@@ -0,0 +1,16 @@
+package nz.co.incognito.record;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainHandler implements RequestHandler {
+    public static Logger LOG = LoggerFactory.getLogger(MainHandler.class);
+
+    @Override
+    public Object handleRequest(Object input, Context context) {
+        LOG.info(input.toString(), context.toString());
+        return null;
+    }
+}
---
+++ src/main/java/nz/co/incognito/record/config/Configuration.java
@@ -0,0 +1,4 @@
+package nz.co.incognito.record.config;
+
+public class Configuration {
+}
